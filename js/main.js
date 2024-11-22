a65dc4b0562562598039fa45517dcea33989a36f
Hanna <hannaaosterberg@hotmail.com>
2024-11-22 16:50:31 +0100

    added about page with json data and started on projects

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@ const menuIcons = document.querySelector(".nav-icons");
 const menuItems = document.querySelector(".nav-items");
 
 const homePage = document.querySelector(".Home");
-const AboutPage = document.querySelector(".About");
+const aboutPage = document.querySelector(".About");
 const projectsPage = document.querySelector(".Projects");
 const contactPage = document.querySelector(".Contact");
 
+const workExperience = document.querySelector(".jobs-container");
+const educationExperience = document.querySelector(".education-container")
 
 
+
+const resume = "resume.json";
 
 menuBtn.addEventListener("click", () => {
     
@@ -22,9 +26,6 @@ menuBtn.addEventListener("click", () => {
     
 })
 
-
-const resume = "resume.json";
-
 async function getCV() {
     try {
         const response = await fetch(resume);
@@ -33,12 +34,67 @@ async function getCV() {
             throw new Error("HTTP ERROR status: " + response.status);
         }
         const data = await response.json();
+        console.log(data)
         return data;
 
     }catch(error){
         console.error(error);
     }
 }
+async function getCVDetails(jobs,education) {
+    try {
+    const response = await fetch(`${resume}/${jobs}/${education}`);
+    
+    if(!response.ok) {
+        throw new Error("HTTP ERROR status: " + response.status)
+    }
+    const resumeDetails = await response.json();
+    return resumeDetails;
 
-const cv = getCV();
-console.log(cv);
+    }catch(error){
+        console.error(error);
+}
+}
+
+async function displayCV() {
+
+    const displayResume = await getCV();
+    const jobs = displayResume.jobs;
+    const education = displayResume.education;
+    console.log(jobs)
+
+    jobs.forEach(element => {
+        workExperience.innerHTML += `
+                                    <div class="jobs">
+                                    <p class="work1"><img class="office-icon" src="./img/office_icon.svg" alt="office icon">
+                                    ${element.company} </p>
+                                    <p class="work-1"><img class="location-icon" src="./img/location_icon.svg" alt="location icon">
+                                    ${element.location} </p>
+                                    </div>
+                                    <div class="jobs-start-end">
+                                    <button class="periodBtn">Full Time</button>
+                                    <p class = "period"><img class="calender-icon" src="./img/calender_icon.svg" alt="calender icon">
+                                    ${element.start}</p>
+                                    </div>
+                                    `;
+    });
+    education.forEach(element => {
+        educationExperience.innerHTML += `
+                                    <div class="jobs">
+                                    <p class="work1"><img class="office-icon" src="./img/office_icon.svg" alt="office icon">
+                                    ${element.school} </p>
+                                    <p class="work1"><img src="./img/student_icon.svg" width = "25"alt="Student icon">
+                                    ${element.education} </p>
+                                    <p class="work-1"><img class="location-icon" src="./img/location_icon.svg" alt="location icon">
+                                    ${element.location} </p>
+                                    </div>
+                                    <div class="jobs-start-end">
+                                    <button class="periodBtn">Full Time</button>
+                                    <p class = "period"><img class="calender-icon" src="./img/calender_icon.svg" alt="calender icon">
+                                    ${element.start}</p>
+                                    </div>
+                                    `;
+    });
+
+}
+displayCV();
